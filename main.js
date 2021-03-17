8cf8c3c087a50cad7f1eaa7e227b8dbe6991f657
Superredstone <patrickcanal3@gmail.com>
2021-03-17 14:05:02 +0100

    Added social category

--- main.js
+++
@@ -1,9 +0,0 @@
-const splash = document.querySelector(".splash")
-
-$time = 2000;
-
-document.addEventListener("DOMContentLoaded", (e)=>{
-    setTimeout(()=>{
-        splash.classList.add("display-none");
-    }, $time);
-})
